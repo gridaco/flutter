0db2a4eec636a9b343535be4c851233b3ef2fc4a
softmarshmallow <woojoo@softmarshmallow.com>
2021-01-13 11:21:39 +0900

    updated export syntax for webpack compatibility

--- lib/dart/index.ts
+++ lib/dart/index.ts
@@ -1,1 +1,7 @@
-export { double, Double } from "./double";
+export {
+    Double,
+} from "./double";
+
+// learn more at https://github.com/webpack/webpack/issues/7378#issuecomment-492641148
+import { double as d } from "./double"
+export type double = d;

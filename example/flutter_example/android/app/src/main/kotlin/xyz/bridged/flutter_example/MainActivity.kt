f152c46b705cc4c2230609cf00954a3816983e9d
softmarshmallow <woojoo@softmarshmallow.com>
2021-03-22 18:28:39 +0900

    remove flutter built example project & moved to bridgedxyz/examples

--- example/flutter_example/android/app/src/main/kotlin/xyz/bridged/flutter_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package xyz.bridged.flutter_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- example/flutter_example/lib/container.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-test(){
-  Container();
-}

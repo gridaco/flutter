e97a75c34f913940333694ade3d8737a44275a9a
softmarshmallow <woojoo@softmarshmallow.com>
2021-05-01 08:19:53 +0900

    bump ver, rm coli

--- lib/index.ts
+++ lib/index.ts
@@ -1,9 +1,12 @@
-export * from "./widgets"
-export * from "./dart-ui"
-export * from "./dart"
-export * from "./vector-math-64"
-export * from "./painting"
-export * from "./material"
-export * from "./rendering"
-export * from "./dynamic"
-export * from "./composer"
+export { Snippet } from "./builder/buildable-tree";
+
+export * from "./widgets";
+export * from "./dart-ui";
+export * from "./dart";
+export * from "./vector-math-64";
+export * from "./painting";
+export * from "./material";
+export * from "./rendering";
+export * from "./dynamic";
+export * from "./composer";
+export * from "./builder";
--- lib/widgets/index.ts
+++ lib/widgets/index.ts
@@ -1,57 +1,59 @@
-import { Widget } from "./widget"
-import { Align } from "./align"
-import { BuildContext } from "./build-context"
-import { Center } from './center'
+import { Widget } from "./widget";
+import { Align } from "./align";
+import { BuildContext } from "./build-context";
+import { Center } from "./center";
 import { Column } from "./column";
 import { Container } from "./container";
-import { DefaultTextStyle } from "./default-text-style"
+import { DefaultTextStyle } from "./default-text-style";
 import { Expanded } from "./expanded";
 import { GestureDetector } from "./gesture-detector";
 import { ImageLoadingBuilder } from "./image-loading-builder";
-import { Image } from "./image"
-import { Icon } from './icon'
-import { MediaQuery } from "./media-query"
-import { Opacity } from "./opacity"
-import { Padding } from "./padding"
-import { Positioned } from "./positioned"
-import { RichText } from "./rich-text"
+import { Image } from "./image";
+import { Icon } from "./icon";
+import { IconData } from "./icon-data";
+import { MediaQuery } from "./media-query";
+import { Opacity } from "./opacity";
+import { Padding } from "./padding";
+import { Positioned } from "./positioned";
+import { RichText } from "./rich-text";
 import { Row } from "./row";
 import { SingleChildScrollView } from "./single-child-scroll-view";
 import { SizedBox } from "./sizedbox";
 import { Spacer } from "./spacer";
 import { Text } from "./text";
-import { ToolbarOptions } from "./toolbar-options"
+import { ToolbarOptions } from "./toolbar-options";
 import { Stack } from "./stack";
 import { Transform } from "./transform";
 import { Visibility } from "./visibility";
-import { WidgetBuilder } from "./widget-builder"
+import { WidgetBuilder } from "./widget-builder";
 
 export {
-    Align,
-    BuildContext,
-    Center,
-    Column,
-    Container,
-    DefaultTextStyle,
-    Expanded,
-    GestureDetector,
-    ImageLoadingBuilder,
-    Image,
-    Icon,
-    MediaQuery,
-    Opacity,
-    Padding,
-    Positioned,
-    RichText,
-    Row,
-    SingleChildScrollView,
-    SizedBox,
-    Spacer,
-    Stack,
-    Text,
-    ToolbarOptions,
-    Transform,
-    Visibility,
-    Widget,
-    WidgetBuilder
-}
+  Align,
+  BuildContext,
+  Center,
+  Column,
+  Container,
+  DefaultTextStyle,
+  Expanded,
+  GestureDetector,
+  ImageLoadingBuilder,
+  Image,
+  Icon,
+  IconData,
+  MediaQuery,
+  Opacity,
+  Padding,
+  Positioned,
+  RichText,
+  Row,
+  SingleChildScrollView,
+  SizedBox,
+  Spacer,
+  Stack,
+  Text,
+  ToolbarOptions,
+  Transform,
+  Visibility,
+  Widget,
+  WidgetBuilder,
+};

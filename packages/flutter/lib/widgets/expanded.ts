0a42ba14a13af8505ebaeaafd027329a3019c4d0
softmarshmallow <woojoo@softmarshmallow.com>
2021-09-30 19:02:28 +0900

    add Expanded

--- packages/flutter/lib/widgets/expanded.ts
+++ packages/flutter/lib/widgets/expanded.ts
@@ -1,11 +1,19 @@
-import { Widget } from ".";
+import { Widget } from "./widget";
 
 /**
  * https://api.flutter.dev/flutter/widgets/Expanded-class.html
  */
 export class Expanded extends Widget {
-    child: Widget
-    constructor() {
-        super()
-    }
+  child: Widget;
+  flex: number;
+
+  /**
+   * https://api.flutter.dev/flutter/widgets/Expanded/Expanded.html
+   */
+  constructor({ flex = 1, child }: { flex?: number; child: Widget }) {
+    super();
+
+    this.child = child;
+    this.flex = flex;
+  }
 }

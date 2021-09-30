51e56a9ffaa2db41078fad95faae99f6f70ab70b
softmarshmallow <woojoo@softmarshmallow.com>
2021-09-30 19:09:13 +0900

    add Flexible

---
+++ packages/flutter/lib/widgets/flexible.ts
@@ -0,0 +1,35 @@
+import { Widget } from ".";
+import { Key } from "../foundation";
+import { FlexFit } from "../rendering";
+
+/**
+ * https://api.flutter.dev/flutter/widgets/Flexible-class.html
+ */
+export class Flexible extends Widget {
+  key?: Key;
+  flex?: number;
+  fit: FlexFit;
+  child: Widget;
+
+  /**
+   * https://api.flutter.dev/flutter/widgets/Flexible/Flexible.html
+   */
+  constructor({
+    key,
+    flex = 1,
+    fit = FlexFit.loose,
+    child,
+  }: {
+    key?: Key;
+    flex?: number;
+    fit?: FlexFit;
+    child: Widget;
+  }) {
+    super();
+
+    this.key = key;
+    this.flex = flex;
+    this.fit = fit;
+    this.child = child;
+  }
+}

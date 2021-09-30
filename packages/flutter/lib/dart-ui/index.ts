1917306bb9e7ba3c825261f3d5f06443ef30575c
softmarshmallow <woojoo@softmarshmallow.com>
2021-09-30 19:30:30 +0900

    sanitize exports

--- packages/flutter/lib/dart-ui/index.ts
+++ packages/flutter/lib/dart-ui/index.ts
@@ -1,30 +1,36 @@
-import { ColorFilter } from "./color-filter"
-import { TileMode } from "./tile-mode";
+import { BlendMode } from "./blend-mode";
+import { ColorFilter } from "./color-filter";
 import { Color } from "./color";
+import { Clip } from "./clip";
 import { FontStyle } from "./font-style";
 import { FontWeight } from "./font-weight";
+import { ImageFilter } from "./image-filter";
 import { Offset } from "./offset";
 import { Radius } from "./radius";
-import { Rect } from "./rect"
+import { Rect } from "./rect";
 import { Size } from "./size";
+import { TileMode } from "./tile-mode";
 import { TextAlign } from "./text-align";
 import { TextDecoration } from "./text-decoration";
-import { TextDecorationStyle } from "./text-decoration-style"
+import { TextDecorationStyle } from "./text-decoration-style";
 import { TextDirection } from "./text-direction";
-import { VoidCallback } from "./void-callback"
+import { VoidCallback } from "./void-callback";
 export {
-    ColorFilter,
-    Color,
-    FontStyle,
-    FontWeight,
-    Offset,
-    Radius,
-    Rect,
-    Size,
-    TextAlign,
-    TextDecoration,
-    TextDecorationStyle,
-    TextDirection,
-    TileMode,
-    VoidCallback
-}
+  BlendMode,
+  ColorFilter,
+  Color,
+  Clip,
+  FontStyle,
+  FontWeight,
+  ImageFilter,
+  Offset,
+  Radius,
+  Rect,
+  Size,
+  TextAlign,
+  TextDecoration,
+  TextDecorationStyle,
+  TextDirection,
+  TileMode,
+  VoidCallback,
+};

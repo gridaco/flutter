3f7d33573d3a393c2eb816d656cea633b95ecc17
softmarshmallow <woojoo@softmarshmallow.com>
2021-09-30 19:02:52 +0900

    add foundation

---
+++ packages/flutter/lib/foundation/key.ts
@@ -0,0 +1,16 @@
+/**
+ * https://api.flutter.dev/flutter/foundation/Key-class.html
+ */
+export class Key {
+  readonly value: string;
+  /**
+   * https://api.flutter.dev/flutter/foundation/Key/Key.html
+   */
+  constructor(value: string) {
+    this.value = value;
+  }
+  /**
+   * https://api.flutter.dev/flutter/foundation/Key/Key.empty.html
+   */
+  static empty() {}
+}

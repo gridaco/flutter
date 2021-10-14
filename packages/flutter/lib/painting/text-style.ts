80cd3c9af3e517eb433e237ece018f6b3cf6a8ec
You-j <ijyk3061@gmail.com>
2021-10-14 15:46:37 +0900

    add height in text-style

--- packages/flutter/lib/painting/text-style.ts
+++ packages/flutter/lib/painting/text-style.ts
@@ -8,63 +8,65 @@ import { TextBaseline } from "./text-baseline";
 
 /**
  * https://api.flutter.dev/flutter/painting/TextStyle-class.html
- * 
+ *
  */
 export class TextStyle extends BuildableTree {
-    color?: Color
-    fontSize?: double
-    fontWeight?: FontWeight
-    fontStyle?: FontStyle
-    fontFamily?: string
-    decoration?: TextDecoration
-    letterSpacing?: double
-    constructor(args?: {
-        color?: Color
-        fontSize?: double
-        fontWeight?: FontWeight
-        fontStyle?: FontStyle
-        fontFamily?: string
-        decoration?: TextDecoration
-        letterSpacing?: double
-    }) {
-        super();
-        this.color = args?.color;
-        this.fontSize = args?.fontSize;
-        this.fontWeight = args?.fontWeight;
-        this.fontStyle = args?.fontStyle;
-        this.fontFamily = args?.fontFamily
-        this.decoration = args?.decoration
-        this.letterSpacing = args?.letterSpacing
-    }
+  color?: Color;
+  fontSize?: double;
+  fontWeight?: FontWeight;
+  fontStyle?: FontStyle;
+  fontFamily?: string;
+  decoration?: TextDecoration;
+  height?: double;
+  letterSpacing?: double;
+  constructor(args?: {
+    color?: Color;
+    fontSize?: double;
+    fontWeight?: FontWeight;
+    fontStyle?: FontStyle;
+    fontFamily?: string;
+    decoration?: TextDecoration;
+    height?: double;
+    letterSpacing?: double;
+  }) {
+    super();
+    this.color = args?.color;
+    this.fontSize = args?.fontSize;
+    this.fontWeight = args?.fontWeight;
+    this.fontStyle = args?.fontStyle;
+    this.fontFamily = args?.fontFamily;
+    this.decoration = args?.decoration;
+    this.height = args?.height;
+    this.letterSpacing = args?.letterSpacing;
+  }
 
-
-    /**
-     * https://api.flutter.dev/flutter/painting/TextStyle/copyWith.html
-     */
-    copyWith(args: {
-        inherit?: boolean,
-        color?: Color,
-        backgroundColor?: Color,
-        fontFamily?: string,
-        fontFamilyFallback?: Array<string>,
-        fontSize?: double,
-        fontWeight?: FontWeight,
-        fontStyle?: FontStyle,
-        letterSpacing?: double,
-        wordSpacing?: double,
-        textBaseline?: TextBaseline,
-        height?: double,
-        // Locale? locale,
-        // Paint? foreground,
-        // Paint? background,
-        // List<Shadow>? shadows,
-        // List<FontFeature>?fontFeatures,
-        decoration?: TextDecoration,
-        decorationColor?: Color,
-        decorationStyle?: TextDecorationStyle,
-        decorationThickness?: double,
-        debugLabel?: string
-    }): this {
-        return this.extendWithExtensionFunction('copyWith', args)
-    }
+  /**
+   * https://api.flutter.dev/flutter/painting/TextStyle/copyWith.html
+   */
+  copyWith(args: {
+    inherit?: boolean;
+    color?: Color;
+    backgroundColor?: Color;
+    fontFamily?: string;
+    fontFamilyFallback?: Array<string>;
+    fontSize?: double;
+    fontWeight?: FontWeight;
+    fontStyle?: FontStyle;
+    letterSpacing?: double;
+    wordSpacing?: double;
+    textBaseline?: TextBaseline;
+    height?: double;
+    // Locale? locale,
+    // Paint? foreground,
+    // Paint? background,
+    // List<Shadow>? shadows,
+    // List<FontFeature>?fontFeatures,
+    decoration?: TextDecoration;
+    decorationColor?: Color;
+    decorationStyle?: TextDecorationStyle;
+    decorationThickness?: double;
+    debugLabel?: string;
+  }): this {
+    return this.extendWithExtensionFunction("copyWith", args);
+  }
 }

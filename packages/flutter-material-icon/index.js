f7849370c1828c45822faeab7bcaed11a57512ba
softmarshmallow <woojoo@softmarshmallow.com>
2021-11-03 00:37:59 +0900

    support cjs for `@flutter-builder/flutter-material-icons`

--- packages/flutter-material-icon/index.js
+++ packages/flutter-material-icon/index.js
@@ -1,16 +1,17 @@
-import { IconData } from "@flutter-builder/flutter";
+"use strict";
 
+const flutter = require("@flutter-builder/flutter");
 /**
  * retrives the flutter `Icons.icon_name` code snippet via name of the material icon
  * uses dynamic tools/python_utils/material_icon_parser at https://github.com/bridgedxyz/dynamic
  * @param name name of the material icon. i.e. add, person_add
  * https://api.flutter.dev/flutter/material/Icons-class.html
  */
-export const Icons = new Proxy(
+const Icons = new Proxy(
   {},
   {
     get: function (target, property, receiver) {
-      return new IconData().overrideSnippet("Icons." + property);
+      return new flutter.IconData().overrideSnippet("Icons." + property);
     },
   }
 );
@@ -19,3 +20,5 @@ export const Icons = new Proxy(
 // - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy/Proxy/get
 // - https://www.reddit.com/r/typescript/comments/q5zeph/possible_solution_for_dynamically_populating_the/
 // - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy/revocable
+
+module.exports.Icons = Icons;

1c2b12a8efd0029e3c8b527a7b8eb938bf61ac03
softmarshmallow <woojoo@softmarshmallow.com>
2022-07-27 16:41:16 +0900

    add assertion

--- packages/flutter/lib/widgets/expanded.ts
+++ packages/flutter/lib/widgets/expanded.ts
@@ -1,4 +1,5 @@
 import { Widget } from "./widget";
+import assert from "assert";
 
 /**
  * https://api.flutter.dev/flutter/widgets/Expanded-class.html
@@ -12,6 +13,8 @@ export class Expanded extends Widget {
    */
   constructor({ flex = 1, child }: { flex?: number; child: Widget }) {
     super();
+
+    assert(child);
 
     this.child = child;
     this.flex = flex;

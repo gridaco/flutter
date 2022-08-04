9b7f5c0b46fb6c50461e1d625c0a753b1c7b7416
softmarshmallow <woojoo@softmarshmallow.com>
2022-08-04 14:26:27 +0900

    init flutter-daemon package

---
+++ packages/flutter-daemon/README.md
@@ -0,0 +1,11 @@
+# Flutter Daemon & WS Server
+
+## APIs
+
+- writeFile
+- createProject
+- run
+
+## References
+
+- https://github.com/flutter/flutter/blob/master/packages/flutter_tools/doc/daemon.md
---
+++ packages/flutter-daemon/index.ts
@@ -0,0 +1,31 @@
+import { Daemon } from "./daemon";
+import ws from "ws";
+
+// Creating a new websocket server
+const wss = new ws.Server({ port: 8080 });
+
+const d = new Daemon();
+d.start();
+d.version();
+d.getDevices();
+d.restart({
+  appId: "0",
+});
+
+// Creating connection using websocket
+wss.on("connection", (ws) => {
+  console.log("new client connected");
+  // sending message
+  ws.on("message", (data) => {
+    console.log(`Client has sent us: ${data}`);
+  });
+  // handling what to do when clients disconnects from server
+  ws.on("close", () => {
+    console.log("the client has connected");
+  });
+  // handling client connection error
+  ws.onerror = function () {
+    console.log("Some Error occurred");
+  };
+});
+console.log("The WebSocket server is running on port 8080");

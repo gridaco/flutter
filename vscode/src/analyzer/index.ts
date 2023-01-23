3c62f06c00fcf59004e6d9f60109912fedd9052f
Universe <woojoo@softmarshmallow.com>
2023-01-23 17:29:09 +0900

    panels

---
+++ vscode/src/analyzer/index.ts
@@ -0,0 +1,32 @@
+import * as vscode from "vscode";
+
+export class Analyzer {
+  constructor(readonly document: vscode.TextDocument) {}
+
+  async widgets(): Promise<WidgetAnalysis[]> {
+    const text = await this.document.getText();
+    const re = /class\s+(\w+)\s+extends\s+StatelessWidget/;
+    const matches = text.match(re);
+
+    if (!matches) {
+      return [];
+    }
+
+    const name = matches[1];
+    const start = matches.index ?? 0;
+
+    return [
+      {
+        start,
+        id: "1",
+        name,
+      },
+    ];
+  }
+}
+
+interface WidgetAnalysis {
+  start: number;
+  id: string;
+  name: string;
+}
--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -1,25 +1,73 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { Analyzer } from "./analyzer";
+
+const langs = ["dart"] as const;
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  let disposable_dart_preview = vscode.commands.registerCommand(
+    "grida-flutter.showPreview",
+    cmd_dart_preview_handler
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "grida-flutter" is now active!');
+  vscode.languages.registerCodeLensProvider(langs, {
+    provideCodeLenses: async (document: vscode.TextDocument) => {
+      const analyzer = new Analyzer(document);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('grida-flutter.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from grida-flutter!');
-	});
+      const components = await analyzer.widgets();
+      return components.map((c) => {
+        const start = document.positionAt(c.start + 2);
+        const lens = new vscode.CodeLens(new vscode.Range(start, start), {
+          command: "grida-flutter.showPreview",
+          arguments: [document, c.id],
+          title: `⚡️ Preview ${c.name}`,
+        });
+        return lens;
+      });
+    },
+  });
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable_dart_preview);
+}
+
+async function cmd_dart_preview_handler(
+  document?: vscode.TextDocument,
+  componentId?: string
+) {
+  const panel_title = `Preview: ${componentId}`;
+
+  // The code you place here will be executed every time your command is executed
+  // Display a message box to the user
+  vscode.window.showInformationMessage("Hello World from grida-flutter!");
+  const panel = vscode.window.createWebviewPanel(
+    "flutter-preview", // Identifies the type of the webview. Used internally
+    panel_title, // Title of the panel displayed to the user
+    vscode.ViewColumn.Beside, // Editor column to show the new webview panel in.
+    {} // Webview options. More on these later.
+  );
+
+  const text = await document?.getText();
+
+  panel.webview.html = getWebviewContent(text ?? "");
+  panel.webview.onDidReceiveMessage((e) => {
+    //
+  });
+}
+
+function getWebviewContent(src: string) {
+  return `<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Document</title>
+	</head>
+	<body>
+		<h1>hello world</h1>
+		<p>${src}</p>
+	</body>
+	</html>`;
 }
 
 // This method is called when your extension is deactivated

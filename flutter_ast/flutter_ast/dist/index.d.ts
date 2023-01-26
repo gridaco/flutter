48f57f6f5907fdb3a5ada886448c06262736cc15
softmarshmallow <woojoo@softmarshmallow.com>
2023-01-27 01:14:43 +0900

    update flutter-ast typings

--- flutter_ast/flutter_ast/dist/index.d.ts
+++ flutter_ast/flutter_ast/dist/index.d.ts
@@ -1,15 +1,97 @@
 // Type definitions for dart-style
 
 interface DartResult {
-  file?: {
-    path: string;
-    imports: Array<string>;
-    classes: Array<object>;
-    methods: Array<object>;
-    enums: Array<object>;
-    fields: Array<object>;
-  };
+  file?: DartFile;
   errors?: Array<object>;
+}
+
+interface DartFile {
+  path: string;
+  classes: Array<DartClass>;
+  enums: Array<DartEnum>;
+  fields: Array<DartField>;
+  imports: Array<string>;
+  methods: Array<DartMethod>;
+}
+
+interface DartClass {
+  constructors: Array<DartConstructor>;
+  comments: Array<DartComment>;
+  fields: Array<DartField>;
+  methods: Array<DartMethod>;
+  name: string;
+  isAbstract?: boolean;
+  extendsClause?: string;
+  implementsClause?: string;
+  withClause?: string;
+}
+
+interface DartConstructor {
+  name: string;
+  properties: Array<DartProperty>;
+}
+
+interface DartProperty extends DartCore {}
+
+interface DartField extends DartCore {}
+
+interface DartComment {
+  lines: Array<string>;
+}
+
+interface DartEnum {
+  name: string;
+  values: Array<string>;
+}
+
+interface DartCore {
+  name?: string;
+  key?: string;
+  type?: string;
+  value?: DartCore;
+
+  /**
+   * @default true
+   */
+  isOptional: boolean;
+
+  /**
+   * @default false
+   */
+  isNamed: boolean;
+
+  /**
+   * @default false
+   */
+  isPositional: boolean;
+
+  /**
+   * @default false
+   */
+  isRequiredPositional: boolean;
+
+  /**
+   * @default false
+   */
+  isSynthetic: boolean;
+
+  /**
+   * @default false
+   */
+  isRequiredNamed: boolean;
+
+  /**
+   * @default false
+   */
+  isOptionalNamed: boolean;
+
+  position?: number;
+}
+
+interface DartMethod {
+  name?: string;
+  parameters?: Array<object>;
+  body;
 }
 
 export function parse(source: string): DartResult;

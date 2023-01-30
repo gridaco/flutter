20a34e2670bc2cd4eecf9ca119d6ef664cfe625b
softmarshmallow <woojoo@softmarshmallow.com>
2023-01-30 16:42:51 +0900

    update example file resolution

--- flutter_ast/flutter_ast/examples/example.dart
+++ flutter_ast/flutter_ast/examples/example.dart
@@ -1,9 +1,13 @@
 import 'package:flutter_ast/flutter_ast.dart';
 import 'dart:io';
 import 'dart:convert';
+import "package:path/path.dart" show dirname, join;
 
 // read file from ./samples/sample.dart
-final String source = File("../samples/sample.dart").readAsStringSync();
+
+final String source =
+    File(join(dirname(Platform.script.path), "../samples/sample.dart"))
+        .readAsStringSync();
 
 // main
 void main(params) {

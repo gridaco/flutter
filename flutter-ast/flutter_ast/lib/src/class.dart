4035b989ef4dbe3fc20b6de052b5c38d40e67f2d
Universe <woojoo@softmarshmallow.com>
2023-02-02 21:45:47 +0900

    flutter ast: fix constructor properties (with types)

--- flutter-ast/flutter_ast/lib/src/class.dart
+++ flutter-ast/flutter_ast/lib/src/class.dart
@@ -3,34 +3,47 @@ import 'index.dart';
 
 extension ClauseDeclarationImplUtils on ClassDeclarationImpl {
   DartClass toDartClass(DartFile parent) {
-    DartClass base = DartClass(
-        name: this.name.toString(), offset: this.offset, end: this.end);
+    var base = DartClass(
+      //
+      name: this.name.toString(),
+      offset: this.offset,
+      end: this.end,
+      isAbstract: this.abstractKeyword != null,
+      extendsClause: this.extendsClause?.toString(),
+      implementsClause: this.implementsClause?.toString(),
+      withClause: this.withClause?.toString(),
+    );
+
+    // (-1) handle fields ( execution order matters. the fields are referenced when handling the constructors )
     final List<DartField?> fields = [];
     for (final item in this.childEntities.whereType<FieldDeclarationImpl>()) {
       fields.add(item.toDartField());
     }
+    base = base.copyWith(fields: fields);
+
+    // (+1) handle constructors
     final List<DartConstructor?> constructors = [];
     for (final item
         in this.childEntities.whereType<ConstructorDeclarationImpl>()) {
       constructors.add(item.toDartConstructor(base));
     }
+    base = base.copyWith(constructors: constructors);
+
+    // (0) handle methods
     final List<DartMethod> methods = [];
     for (final item in this.childEntities.whereType<MethodDeclarationImpl>()) {
       methods.add(item.toDartMethod(base));
     }
+    base = base.copyWith(methods: methods);
+
+    // (0) handle comments
     final List<DartComment> comments = [];
     for (final item in this.childEntities.whereType<CommentImpl>()) {
       comments.add(item.toDartComment());
     }
-    return base.copyWith(
-      isAbstract: this.abstractKeyword != null,
-      extendsClause: this.extendsClause?.toString(),
-      implementsClause: this.implementsClause?.toString(),
-      withClause: this.withClause?.toString(),
-      fields: fields,
-      constructors: constructors,
-      methods: methods,
-      comments: comments,
-    );
+    base = base.copyWith(comments: comments);
+
+    // return
+    return base;
   }
 }
--- flutter-ast/flutter_ast/lib/src/constructor.dart
+++ flutter-ast/flutter_ast/lib/src/constructor.dart
@@ -2,9 +2,10 @@ import 'package:flutter_ast_core/flutter_ast_core.dart';
 import 'index.dart';
 
 extension ConstructorDeclarationImplUtils on ConstructorDeclarationImpl {
-  DartConstructor? toDartConstructor(DartClass parent) {
-    DartConstructor? base;
+  DartConstructor toDartConstructor(DartClass parent) {
     String _name = '';
+    List<DartProperty> _properties = [];
+
     for (final node in this.childEntities) {
       if (node is SimpleIdentifierImpl) {
         _name = node.name;
@@ -12,17 +13,24 @@ extension ConstructorDeclarationImplUtils on ConstructorDeclarationImpl {
       if (node is DeclaredSimpleIdentifier) {
         _name = node.name;
       }
-      base = DartConstructor(name: _name, offset: this.offset, end: this.end);
+
       if (node is FormalParameterListImpl) {
         for (final child in node.childEntities) {
           if (child is DefaultFormalParameterImpl) {
-            final _props = List<DartProperty?>.from(base!.properties);
+            final _props = List<DartProperty?>.from(_properties);
             _props.add(child.toDartProperty(parent.fields));
-            base = base.copyWith(properties: _props);
+            _properties = List<DartProperty>.from(_props);
           }
         }
       }
     }
+
+    final base = DartConstructor(
+        name: _name,
+        offset: this.offset,
+        end: this.end,
+        properties: _properties);
+
     return base;
   }
 }
